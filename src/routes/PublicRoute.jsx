9908dfa645d0ece03066cbda0e8391ec60304652
Ahmed Mohammed <109735125+ahmedmoahmed00@users.noreply.github.com>
2025-12-16 13:40:25 +0200

    Fix Bug

--- src/routes/PublicRoute.jsx
+++ src/routes/PublicRoute.jsx
@@ -7,16 +7,16 @@ function PublicRoute({ children }) {
   const { isLoading, isAuthenticated, user } = useUser();
   const navigate = useNavigate();
 
-  console.log(user);
   useEffect(() => {
     if (
       !isLoading &&
       isAuthenticated &&
       !(user?.recovery_sent_at && !user?.password_changed_at)
-    )
-      console.log("Valid");
-    console.log(user);
-    navigate("/");
+    ) {
+      console.log("Valid user, redirecting to home");
+      console.log("user", user);
+      navigate("/");
+    }
   }, [isAuthenticated, isLoading, user, navigate]);
 
   if (isLoading) return <Loader />;
